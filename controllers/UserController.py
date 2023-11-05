edcbd54ea272c3303324d5531f4dc96e4550f091
hmnhGeek <hmnhsharma97@gmail.com>
2023-11-05 22:35:42 +0530

    Refactored code for access token

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -15,27 +15,30 @@ class UserController:
 
     @user_controller_router.post("/register")
     def register(self, user: User):
-        usr = self.userService.register(user)
-
-        if usr is not False:
-            return {"message": f"User {user.username} registered successfully!"}
-        else:
-            raise HTTPException(status_code=422, detail="User already exists")
+        try:
+            usr = self.userService.register(user)
+            if usr is not False:
+                return CustomResponseMessage(status_code = 200, message = f"User {user.username} registered successfully.")
+            else:
+                raise HTTPException(status_code=422, detail="User already exists")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
     
     @user_controller_router.post("/token")
     def get_access_token(self, form_data: OAuth2PasswordRequestForm = Depends()):
-        if not self.userService.is_session_active(form_data.username):
+        try:
             access_token = self.userService.get_access_token(form_data)
-
-            if access_token is not None:
-                self.userService.register_token_in_session(access_token)
-                return {"access_token": access_token, "token_type": "bearer"}
-            else: raise HTTPException(status_code=400, detail="Incorrect username or password")
-        else:
-            return CustomResponseMessage(status_code = 200, message = "Session already active")
+            if access_token["access_token"] is None:
+                raise HTTPException(status_code=400, detail="Incorrect username or password")
+            return access_token
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
     @user_controller_router.post("/logout")
     def logout(self, token: str = Header(None)):
-        if token is None: raise HTTPException(status_code=401, detail="Invalid token")
-        self.userService.logout(token)
-        return CustomResponseMessage(status_code = 200, message = "Logged out successfully")
+        try:
+            if token is None: raise HTTPException(status_code=401, detail="Invalid token")
+            self.userService.logout(token)
+            return CustomResponseMessage(status_code = 200, message = "Logged out successfully")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
